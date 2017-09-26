26d704a5e4556737cf8045d6e310e860c261b29f
Oruj <oruc.mehdi@gmail.com>
2017-09-26 10:04:37 +0200

    Implement Games

---
+++ Desktop/JAVA/salvo/src/main/java/salvo/salvo/Game.java
@@ -0,0 +1,46 @@
+package salvo.salvo;
+
+import com.sun.swing.internal.plaf.synth.resources.synth_sv;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class Game {
+
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Long Id;
+    private Date creationDate = new Date();
+
+    @OneToMany(mappedBy="game", fetch=FetchType.EAGER)
+    Set<GamePlayer> gameplayer = new HashSet<>();
+
+    public Game() {
+    }
+
+    public Game(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Date getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(Date creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+}
+
+
---
+++ Desktop/JAVA/salvo/src/main/java/salvo/salvo/GamePlayer.java
@@ -0,0 +1,59 @@
+package salvo.salvo;
+
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import java.util.Date;
+import java.util.Set;
+import java.util.HashSet;
+
+@Entity
+public class GamePlayer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long Id;
+    private Date joinDate;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "Player_Id")
+    private Player player;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "Game_Id")
+    private Game game;
+
+    public GamePlayer(Player p1, Game g1) {
+        this.player = player;
+        this.game = game;
+        this.joinDate = new Date();
+    }
+
+    public GamePlayer() {
+
+    }
+
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+}
+
